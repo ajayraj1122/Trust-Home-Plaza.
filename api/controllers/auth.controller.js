3756f1aaf17703ea56feeef6bffa49ffeba6208c
Ajay Raj <ajaygupta9210p@gmail.com>
2024-11-09 18:04:34 +0530

    some changes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -36,7 +36,7 @@ export const signin = async (req, res, next) => {
 export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    if (user) {
+    if (user) {  //if user exist
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
       res
@@ -49,8 +49,8 @@ export const google = async (req, res, next) => {
         Math.random().toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
       const newUser = new User({
-        username:
-          req.body.name.split(' ').join('').toLowerCase() +
+        username:// ajay raj to ajayraj2120u8fdsnh
+          req.body.name.split(" ").join("").toLowerCase() +
           Math.random().toString(36).slice(-4),
         email: req.body.email,
         password: hashedPassword,
