2753c5894c6d8bfa244d60b90cab7c7fc651fa6e
Ajay Raj <ajaygupta9210p@gmail.com>
2025-01-09 18:15:39 +0530

    update header title and about section text; add license to package-lock.json

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,8 +27,9 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Comfort</span>
-            <span className='text-slate-700'>Cove</span>
+            <span className='text-slate-500'>Trust </span>
+            <span className='text-slate-600'> Home</span>
+            <span className='text-slate-700'>Plaza </span>
           </h1>
         </Link>
         <form
